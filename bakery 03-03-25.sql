af6ca32d4354d5dfd7dec2fa04fbacd521dd72cc
unknown <vcbg.110.1977047@gmail.com>
2025-03-04 20:42:17 -0600

    Actulizacion Base De Datos

--- bakery 03-03-25.sql
+++ bakery 03-03-25.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS Employees(
 IDEmployees INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 FirstName VARCHAR (100),
 LastName VARCHAR(100),
-Horario VARCHAR(100)
+TimeTable VARCHAR(100)
 );
 INSERT INTO Products(FirstName, Descripcion, Price)
 VALUES('Pan', 'Alimento escencial elaborado con harina, agua, levadura y sal', 15), 
@@ -38,7 +38,21 @@ VALUES('Pan', 'Alimento escencial elaborado con harina, agua, levadura y sal', 1
 ('Levadura', 'Ingdrediente clave en la panaderia, responsable de hacer que la masa crezca', '300'), 
 ('Harina', 'Base de la mayoria de los productos de panaderia', '150');
 
-INSERT INTO Customers(FirstName, LastName, Email)VALUES('Alexa Natalia', 'Flores Martinez', 'anfm@gmail.com'), ('Jaasiel Alberto', 'Villareal Cano', 'javc@gmail.com')
+INSERT INTO Customers(FirstName, LastName, Email)VALUES('Alexa Natalia', 'Flores Martinez', 'anfm@gmail.com'),
+('Jaasiel Alberto', 'Villareal Cano', 'javc@gmail.com'),
+('Axel Mauricio', 'Villarreal Cano', 'amvc@gmail.com'),
+('Miriam Judith', 'Cano Gallardo', 'mjcg@gmail.com'),
+('Denise Abigail', 'Flores Martinez', 'dafm@gmail.com');
+
+INSERT INTO Orders()VALUES();
+
+INSERT INTO Employees(FirstName, LastName, TimeTable)VALUES('Melody Mariel', 'Alba Cano', '05:00 am - 13:00 pm'), 
+('Rodrigo', 'Fuentes Cano', '07:00 am - 15:00 pm'), 
+('Keila Haniel', 'Alba Cano', '09:00 am - 17:00 pm'), 
+('Sebastian', 'Fuentes Cano', '13:00 pm - 09:00 pm'),
+('Sofia Gisel', 'Alba Cano', '15:00 pm - 11:00 pm');
+
+
 
 
 
